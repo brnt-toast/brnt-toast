20c48c25b388921c60f25f5cdc1216b63142b251
brnt-toast <yoyo.beerme@gmail.com>
2022-04-12 17:13:23 -0400

    bootstrap working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log('hello world')
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import $ from 'jquery';
+import Popper from 'popper.js';
