3c239454d6079955812923e6561240b941833406
brnt-toast <yoyo.beerme@gmail.com>
2022-04-20 16:05:26 -0400

    + background; needs tweaking

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div
+      style={{
+        width: '100vw',
+        height: '100vh',
+        background: "linear-gradient(115deg, #E5B181 50%, #81ADC8 50%)",
+        overflow: "hidden"
+      }}
+    >
+      hello from home.js
+
+    </div>
   )
 }
