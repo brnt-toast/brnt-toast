ebdf98c78d09c404d0625ba06b8c4e1d33bff812
brnt-toast <yoyo.beerme@gmail.com>
2022-04-22 09:35:12 -0400

    clean: styles--Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,9 @@
 import React from 'react'
+import '../styles/Home.sass'
 
 export default function Home() {
   return (
-    <div
-      style={{
-        width: '100vw',
-        height: '100vh',
-        background: "linear-gradient(115deg, #E5B181 50%, #81ADC8 50%)",
-        overflow: "hidden"
-      }}
-    >
+    <div class='Home'>
       hello from home.js
 
     </div>
