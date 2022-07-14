20a5ce07e0ed1fa9774f280bf6c0acb048b8d588
brnt-toast <yoyo.beerme@gmail.com>
2022-07-13 20:54:11 -0400

    init: about page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Routes, Route, Link} from 'react-router-dom'
 
 import Nav from './components/Nav'
 import Home from './pages/Home'
+import About from'./pages/About'
 import './styles/App.sass'
 
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
         <Nav />
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
         </Routes>
       </div>
   )
---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import '../styles/Home.sass'
+
+
+export default function Home() {
+  return (
+    <div className='Home'>
+      <aside>
+        {/* hello from aside */}
+      </aside>
+      <main>
+        {/* hello from main */}
+      </main>
+    </div>
+  )
+}
